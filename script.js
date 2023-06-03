dd5d863dbf47d642c396b6f21c63192875eb6707
eloaandreatta <111518085+eloaandreatta@users.noreply.github.com>
2023-06-03 17:00:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let audios = [
-    {caminho:'audios/grito.mp3', legenda:'GRITO'},
-    {caminho:'audios/jesus.mp3', legenda:'JESUS'},
-    {caminho:'audios/nao-e-o-pai.mp3', legenda:'ELE NÃO É O PAI'},
-    {caminho:'audios/pare.mp3', legenda:'PARE'},
-    {caminho:'audios/que-isso-meu-filho.mp3', legenda:'QUE ISSO MEU FILHO'},
-    {caminho:'audios/ratinhoo.mp3', legenda:'RATINHO'},
-    {caminho:'audios/uepa.mp3', legenda:'UEPA'},
-    {caminho:'audios/vixi.mp3', legenda:'VIXI'},
-    {caminho:'audios/xaropinho.mp3', legenda:'RAPAAA'},
+    {caminho:'grito.mp3', legenda:'GRITO'},
+    {caminho:'jesus.mp3', legenda:'JESUS'},
+    {caminho:'nao-e-o-pai.mp3', legenda:'ELE NÃO É O PAI'},
+    {caminho:'pare.mp3', legenda:'PARE'},
+    {caminho:'que-isso-meu-filho.mp3', legenda:'QUE ISSO MEU FILHO'},
+    {caminho:'ratinhoo.mp3', legenda:'RATINHO'},
+    {caminho:'uepa.mp3', legenda:'UEPA'},
+    {caminho:'vixi.mp3', legenda:'VIXI'},
+    {caminho:'xaropinho.mp3', legenda:'RAPAAA'},
 ];
 
 let botoes = document.querySelectorAll('.botao');
